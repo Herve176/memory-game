89b760b2a23fc458514571ffd647bd0543b0416e
herve <hdjomo1@gmail.com>
2024-03-07 10:28:49 +0100

    visuals added

---
+++ index.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById(btn);
+const element = [];
+let clock = document.getElementById(clock).value;
+
+btn.addEventListener("click" , function(){
+    for(element;i=0;i++){
+        element[i] = document.getElementById(i+1)
+        clock.innerHTML = 5--;
+    }
+})
