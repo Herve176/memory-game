6973f947b889f621c0ca45a05205bf9a8b882ee2
herve <hdjomo1@gmail.com>
2024-03-07 20:48:19 +0100

    just made some new stuff

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-let btn = document.getElementById(btn);
-const element = [];
-let clock = document.getElementById(clock).value;
+let btn = document.getElementById("btn");
+const elements = ['#ff0000', '#008000', '#5733ff', '#ffff00',
+'#ff0000', '#008000', '#5733ff', '#ffff00', 
+'#ff0000', '#008000', '#5733ff', '#ffff00', 
+'#ff0000', '#008000', '#5733ff', '#ffff00' ];
+const shuffled = shuffleArray(elements);
+let tbl = [];
+let clock = document.getElementById("clock");
+let imer = 5;
+const defaul = ['🤖', '🤖', '🤖', '🤖', 
+'🤖', '🤖', '🤖', '🤖', 
+'🤖', '🤖', '🤖', '🤖', 
+'🤖', '🤖', '🤖', '🤖'];
 
-btn.addEventListener("click" , function(){
-    for(element;i=0;i++){
-        element[i] = document.getElementById(i+1)
-        clock.innerHTML = 5--;
-    }
-})
+
+btn.addEventListener("click", function () {
+    setTimeout(coutdown, 5000)
+    function coutdown() {
+        clock.innerHTML = imer;
+        imer--;
+        if (imer == 0) {
+            imer = 5;
+        }
+        alert('time out try to find the Yellow color!🤖')
+    for(let i=0;i<16;i++){
+        if(i<16){
+        tbl[i] = document.getElementById(i+1);
+        tbl[i].innerHTML = shuffled[i] 
+        }else{tbl[i].innerHTML = defaul[i];
+
+        }
+        }
+
+    }})
+
+function shuffleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+    
+
